5d50523eb5181e6b2dd1d77fa0765770be9e7ac3
panda5201 <vinsongautama78@gmail.com>
2025-09-06 17:10:29 +0700

    Commit No. 2 – Result activity [Intent]

---
+++ app/src/main/java/com/example/lab_week_02_b/ResultActivity.kt
@@ -0,0 +1,37 @@
+package com.example.lab_week_02_b
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class ResultActivity : AppCompatActivity() {
+    companion object {
+        const val COLOR_KEY = "COLOR_KEY"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+        val colorCode = intent.getStringExtra(COLOR_KEY)
+
+        val backgroundScreen =
+            findViewById<ConstraintLayout>(R.id.background_screen)
+        val resultMessage =
+            findViewById<TextView>(R.id.color_code_result_message)
+
+        if (!colorCode.isNullOrEmpty()) {
+            try {
+                backgroundScreen.setBackgroundColor(Color.parseColor("#$colorCode"))
+                resultMessage.text = getString(
+                    R.string.color_code_result_message,
+                    colorCode.uppercase()
+                )
+            } catch (e: IllegalArgumentException) {
+                resultMessage.text = getString(R.string.color_code_input_invalid)
+            }
+        }
+    }
+}
